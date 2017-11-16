18313255ab5a752c137004d1ad7dd0f79db142c0
mac <mac@macdeMac-mini.local>
2017-11-16 11:20:22 +0800

    first commit

---
+++ src/LINPackagist/Test.php
@@ -0,0 +1,12 @@
+<?php
+namespace LINPackagist;
+
+class Test {
+    public function abc()
+    {
+        echo 'namespace(' . __NAMESPACE__ . ') - class(' . __CLASS__ . ') - function(' . __FUNCTION__ . ').';
+        echo '<br /> wow ! <br />';
+    }
+}
+
+?>
